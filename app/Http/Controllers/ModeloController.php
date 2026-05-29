5f44707c62bc47d647a0c93fbb3b2aa92de0a4d4
Rosabel <rcaroc@ucvvirtual.edu.pe>
2026-05-29 11:58:29 -0500

    creando controlador de modelo

--- app/Http/Controllers/ModeloController.php
+++ app/Http/Controllers/ModeloController.php
@@ -1,10 +1,20 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Models;
 
-use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model as LaravelModel; // Renombramos el core para evitar conflicto
 
-class ModeloController extends Controller
+class Modelo extends LaravelModel
 {
-    //
+    use HasFactory;
+
+    protected $table = 'modelo'; // Tu tabla en Supabase
+    protected $primaryKey = 'modelo_id';
+    public $timestamps = true;
+
+    protected $fillable = [
+        'modelo_nombre',
+        'modelo_ubicacion'
+    ];
 }
