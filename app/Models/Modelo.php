c8cd1ef516f4dbd9879d74b370f20af8cb9c090a
Rosabel <rcaroc@ucvvirtual.edu.pe>
2026-05-29 12:00:01 -0500

    creando modelo en models

--- app/Models/Modelo.php
+++ app/Models/Modelo.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model as LaravelModel; // Renombramos el core para evitar conflicto
 
-class Modelo extends Model {
-    protected $table = 'modelo';
+class Modelo extends LaravelModel
+{
+    use HasFactory;
+
+    protected $table = 'modelo'; // Tu tabla en Supabase
     protected $primaryKey = 'modelo_id';
+    public $timestamps = true;
+
+    protected $fillable = [
+        'modelo_nombre',
+        'modelo_ubicacion'
+    ];
 }
