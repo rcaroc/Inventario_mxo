ce3b0da1141e3d03327e3811a72e3c25b20aaa60
Rosabel <rcaroc@ucvvirtual.edu.pe>
2026-05-29 15:37:51 -0500

    producto

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -4,12 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Producto extends Model {
+class Producto extends Model
+{
     protected $table = 'producto';
     protected $primaryKey = 'producto_id';
 
+    protected $fillable = [
+        'modelo_id',
+        'color',
+        'talla',
+        'stock'
+    ];
+
     // Relación: Un producto pertenece a un modelo
-    public function modelo() {
-        return $this->belongsTo(Modelo::class, 'modelo_id');
+    public function modelo()
+    {
+        return $this->belongsTo(Modelo::class, 'modelo_id', 'modelo_id');
     }
 }
