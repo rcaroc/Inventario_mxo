7d66cee485ca5d85d60ff4cfc9ff88ea592a1ab4
Rosabel <rcaroc@ucvvirtual.edu.pe>
2026-05-30 10:41:30 -0500

    cambiando datos de env

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        // REGISTRAMOS EL ALIAS AQUÍ
+        
+        // 1. ESTA LÍNEA ES LA QUE QUITA EL AVISO DE "PELIGROSO"
+        // Le dice a Laravel que confíe en el certificado SSL de Render
+        $middleware->trustProxies(at: '*');
+
+        // 2. TUS ALIAS YA EXISTENTES
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
         ]);
